23d11bd126d75c82ac5dcf1741de631ca286e096
N1liaq_ <ailinyanos83@gmail.com>
2026-05-28 00:05:02 -0300

    se cambio el nombre <practicas.js> por <datos-personales> y se agregó su respectivo contenido

---
+++ assets/datos-personales.js
@@ -0,0 +1,9 @@
+const nombre = "Yanet";
+
+let edad = 19,
+  carrera = "Tecnicatura en Desarrollo de Software Multiplataforma",
+  cursada = true;
+
+console.log(
+  `nombre: ${nombre} edad: ${edad} carrera: ${carrera} cursada: ${cursada}`,
+);
